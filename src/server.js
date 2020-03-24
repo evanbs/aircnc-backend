f5bea34c5e32b0087fd18e26cb2d81ce9015ac5f
Evandro Barbosa <evandro.barbosa@yahoo.com.br>
2020-03-24 00:00:08 -0300

    Release 1.0.1

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@aircnc-fjikz.mongodb.net/aircnc?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@aircnc-fjikz.mongodb.net/aircnc?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(process.env.PORT);
